962828ffc815c0ca736ea6c99afb93aa61926bc5
Tyler Li <eustinalwq@gmail.com>
2025-04-22 00:56:28 +0000

    added ray jupyter notebook

--- model_runner.sh
+++ model_runner.sh
@@ -8,25 +8,25 @@ docker compose run trainer
 
 # Step 3: Start Ray head node
 export HOST_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4 )
-docker compose -f docker-compose-ray-cuda.yaml up -d
+docker compose -f docker-compose-ray-1-gpu.yaml up -d
 
 # if got 2 gpu:
 # run on node-mltrain
 docker exec -it ray-worker-0 nvidia-smi --list-gpus
-and
+
 # # run on node-mltrain
 # docker exec -it ray-worker-1 nvidia-smi --list-gpus
 
 
 ## start jupyter container
 # run on node-mltrain
-docker build -t jupyter-ray -f mltrain-chi/docker/Dockerfile.jupyter-ray .
+docker build -t jupyter-ray -f Dockerfile.jupyter-ray .
 Run
 
 # run on node-mltrain
 HOST_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4 )
 docker run  -d --rm  -p 8888:8888 \
-    -v ~/mltrain-chi/workspace_ray:/home/jovyan/work/ \
+    -v ~/workspace_ray:/home/jovyan/work/ \
     -e RAY_ADDRESS=http://${HOST_IP}:8265/ \
     --name jupyter \
     jupyter-ray
