a8f576dc35d351530ebf442d1057b183ac9cab0a
Tyler Li <eustinalwq@gmail.com>
2025-04-22 00:41:01 +0000

    added ray cluster

--- model_runner.sh
+++ model_runner.sh
@@ -1,11 +1,35 @@
 # Step 1: Download + extract dataset
-docker compose run dataset
+docker volume create codeqa
+docker compose -f docker-compose-data.yaml up -d
+docker run --rm -it -v codeqa:/mnt alpine ls -l /mnt
 
 # Step 2: Launch training shell
-docker compose run trainer ### still working on it
+docker compose run trainer 
 
 # Step 3: Start Ray head node
-docker compose run ray-head
+export HOST_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4 )
+docker compose -f docker-compose-ray-cuda.yaml up -d
 
-# Step 4: Start Ray worker node (optional, if needed)
-docker compose run ray-worker
+# if got 2 gpu:
+# run on node-mltrain
+docker exec -it ray-worker-0 nvidia-smi --list-gpus
+and
+# # run on node-mltrain
+# docker exec -it ray-worker-1 nvidia-smi --list-gpus
+
+
+## start jupyter container
+# run on node-mltrain
+docker build -t jupyter-ray -f mltrain-chi/docker/Dockerfile.jupyter-ray .
+Run
+
+# run on node-mltrain
+HOST_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4 )
+docker run  -d --rm  -p 8888:8888 \
+    -v ~/mltrain-chi/workspace_ray:/home/jovyan/work/ \
+    -e RAY_ADDRESS=http://${HOST_IP}:8265/ \
+    --name jupyter \
+    jupyter-ray
+
+# then run
+docker logs jupyter
