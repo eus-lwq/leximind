502e8a1599e1c8ebda87360a3eb226834eff2603
Tyler Li <eustinalwq@gmail.com>
2025-04-29 22:08:50 +0000

    update launch

--- launch.sh
+++ launch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Set environment variables
 export HOST_IP=$(curl --silent http://169.254.169.254/latest/meta-data/public-ipv4)
 git clone https://github.com/Yuan-33/llama-factory.git
@@ -29,16 +28,13 @@ docker cp ray_submit.py llama-trainer:/llama-factory/ray_submit.py
 echo "Waiting for Jupyter to start..."
 sleep 5
 docker logs jupyter 2>&1 | grep "http://127.0.0.1:8888"
-
-docker cp leximind/new_train.sh llama-trainer:/llama-factory/new_train.sh
-docker cp leximind/ray_submit.py llama-trainer:/llama-factory/ray_submit.py
-
 echo "Environment is ready!"
 echo "- Ray dashboard: http://$HOST_IP:8265"
 echo "- Grafana: http://$HOST_IP:3000"
 echo "- MinIO: http://$HOST_IP:9001"
+echo "- Jupyter: http://$HOST_IP:8888"
+echo "- Prometheus: http://$HOST_IP:9090"
 echo "- To connect to llama-trainer: docker exec -it llama-trainer bash"
-echo "- To run container training job directly: docker exec -it llama-trainer bash /llama-factory/train_outside.sh"
-echo "- To submit Ray job: docker exec -it llama-trainer python3 /ray_scripts/ray_submit.py"
-echo "- To monitor training: docker exec -it llama-trainer python3 /ray_scripts/ray_monitor.py"
+echo "- To run train directly with bash: docker exec -it llama-trainer bash train.sh"
+echo "- To submit Ray job: docker exec -it llama-trainer python3 ray_submit.py"
 echo "- To stop the environment: docker compose down"
